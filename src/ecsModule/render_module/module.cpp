eded7fb708c1515278a4126290342bf52f40547d
Tinck41 <glushkov2000@bk.ru>
2026-08-02 15:05:18 +0500

    RenderModule: use SDL_GPU_PRESENTMODE_VSYNC

--- src/ecsModule/render_module/module.cpp
+++ src/ecsModule/render_module/module.cpp
@@ -244,12 +244,13 @@ RenderModule::RenderModule(flecs::world& world) {
 				assert(SDL_ClaimWindowForGPUDevice(render_device.gpu, window.handle) && SDL_GetError());
 
 				SDL_GPUPresentMode presentMode = SDL_GPU_PRESENTMODE_VSYNC;
-				if (SDL_WindowSupportsGPUPresentMode(render_device.gpu, window.handle, SDL_GPU_PRESENTMODE_IMMEDIATE)) {
-					presentMode = SDL_GPU_PRESENTMODE_IMMEDIATE;
-				}
-				else if (SDL_WindowSupportsGPUPresentMode(render_device.gpu, window.handle, SDL_GPU_PRESENTMODE_MAILBOX)) {
-					presentMode = SDL_GPU_PRESENTMODE_MAILBOX;
-				}
+				// TODO
+				//if (SDL_WindowSupportsGPUPresentMode(render_device.gpu, window.handle, SDL_GPU_PRESENTMODE_IMMEDIATE)) {
+				//	presentMode = SDL_GPU_PRESENTMODE_IMMEDIATE;
+				//}
+				//else if (SDL_WindowSupportsGPUPresentMode(render_device.gpu, window.handle, SDL_GPU_PRESENTMODE_MAILBOX)) {
+				//	presentMode = SDL_GPU_PRESENTMODE_MAILBOX;
+				//}
 
 				SDL_SetGPUSwapchainParameters(render_device.gpu, window.handle, SDL_GPU_SWAPCHAINCOMPOSITION_SDR, presentMode);
 			});
